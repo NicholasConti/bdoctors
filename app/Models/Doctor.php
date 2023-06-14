3edf83008bc802f8a8f21b502ee9335fbcda2879
NicholasConti <conti94@live.it>
2023-06-14 16:45:03 +0200

    models

---
+++ app/Models/Doctor.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+    use HasFactory;
+
+    // RELATIONS
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function specializations()
+    {
+        return $this->belongsToMany(Specialization::class);
+    }
+
+    public function sponsorships()
+    {
+        return $this->belongsToMany(Sponsorship::class);
+    }
+
+    public function votes()
+    {
+        return $this->belongsToMany(Vote::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,11 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function doctor()
+    {
+        return $this->hasOne(Doctor::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
