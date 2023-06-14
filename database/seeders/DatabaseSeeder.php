66044ee56fb8826dbfb60215f0a23fd08eae9288
daniloeberli <danilo.eberli@gmail.com>
2023-06-14 17:20:26 +0200

    feat: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            DatabaseSeeder::class,
+            SpecializationSeeder::class,
+            SponsorshipSeeder::class,
+            VotesTableSeeder::class
+        ]);
     }
 }
