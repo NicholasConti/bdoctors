7f8112b3384c636dac64620d4bd1756db841093f
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-06-14 15:48:27 +0200

    users doctors

--- database/migrations/2023_06_14_134419_create_users_table.php
+++ database/migrations/2023_06_14_134419_create_users_table.php
@@ -15,10 +15,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',40);
+            $table->string('surname',40);
+            $table->string('address');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->foreignId('doctor_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
             $table->rememberToken();
             $table->timestamps();
         });
