e1e603ed5e0ef0227409b3ec0c792b91e1718072
daniloeberli <danilo.eberli@gmail.com>
2023-06-14 17:21:39 +0200

    fix: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            DatabaseSeeder::class,
             SpecializationSeeder::class,
             SponsorshipSeeder::class,
             VotesTableSeeder::class
