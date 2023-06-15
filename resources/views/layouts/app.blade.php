0b398b00908b306f460513180cf3ad8997fc880f
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-06-15 10:02:47 +0200

    fix header link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,15 +44,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
-                        </li>
                         @auth
                         <li class="nav-item">
                             <a class="nav-link" href="{{route('admin.dashboard') }}">{{ __('Dashboard') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            {{-- <a class="nav-link" href="{{route('admin.projects.index') }}">{{ __('Projects') }}</a> --}}
                         </li>
                         @endauth
 
