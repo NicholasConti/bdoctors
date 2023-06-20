79ff122aac3bdf98a97315f90906364b9ca61248
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-06-20 16:26:00 +0200

    fix:app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,11 +47,14 @@
                     <ul class="navbar-nav me-auto">
                         {{-- mostra i due link se è un dottore autenticato --}}
                         @if (auth()->user() || !auth()->user()->doctor)
+                        @else
                             <li class="nav-item">
-                                <a class="hov nav-link" href="{{ route('doctor.dashboard') }}">{{ __('Dashboard') }}</a>
+                                <a class="hov nav-link"
+                                    href="{{ route('doctor.dashboard') }}">{{ __('Dashboard') }}</a>
                             </li>
                             <li class="nav-item">
-                                <a class="hov nav-link" href="{{ route('doctor.message') }}">{{ __('My Message') }}</a>
+                                <a class="hov nav-link"
+                                    href="{{ route('doctor.message') }}">{{ __('My Message') }}</a>
                             </li>
                         @endif
                     </ul>
@@ -60,19 +63,26 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('doctor.dashboard') }}">{{__('Dashboard')}}</a>
+                                {{-- <a class="dropdown-item" href="{{ route('admin.') }}">{{__('Profile')}}</a> --}}
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -85,21 +95,10 @@
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item"
-                                            href="{{ route('doctor.dashboard') }}">{{ __('Dashboard') }}</a>
-                                        {{-- <a class="dropdown-item" href="{{ route('admin.') }}">{{__('Profile')}}</a> --}}
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                            class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
                             </li>
                         @endguest
                     </ul>
