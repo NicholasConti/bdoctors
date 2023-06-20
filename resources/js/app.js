f39cc1cde581a42e61d7310171328f4dbabf1849
NicholasConti <conti94@live.it>
2023-06-20 16:05:11 +0200

    image-preview-edit

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,4 +6,17 @@ import.meta.glob([
     '../img/**'
 ])
 
+// image preview
 
+const imageInput = document.querySelector('#image');
+
+imageInput.addEventListener('change', showPreviewImg);
+
+function showPreviewImg() {
+    if (event.target.files.length > 0) {
+        const src = URL.createObjectURL(event.target.files[0]);
+        const preview = document.getElementById("file-image-preview");
+        preview.src = src;
+        preview.style.display = "block";
+    }
+}
