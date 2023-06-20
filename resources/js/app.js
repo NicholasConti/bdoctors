d9fa0dc094ecc3008a9d604ae82d2cfc500d4934
NicholasConti <conti94@live.it>
2023-06-20 16:23:10 +0200

    preview-cv

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,8 +9,10 @@ import.meta.glob([
 // image preview
 
 const imageInput = document.querySelector('#image');
+const cvInput = document.querySelector('#cv');
 
 imageInput.addEventListener('change', showPreviewImg);
+cvInput.addEventListener('change', showPreviewCv);
 
 function showPreviewImg() {
     if (event.target.files.length > 0) {
@@ -18,5 +20,14 @@ function showPreviewImg() {
         const preview = document.getElementById("file-image-preview");
         preview.src = src;
         preview.style.display = "block";
+    }
+}
+
+function showPreviewCv() {
+    if (event.target.files.length > 0) {
+        const src = URL.createObjectURL(event.target.files[0]);
+        const previewCv = document.getElementById("file-cv-preview");
+        previewCv.data = src;
+        previewCv.style.display = "block";
     }
 }
--- resources/views/doctor/dashboard.blade.php
+++ resources/views/doctor/dashboard.blade.php
@@ -219,10 +219,20 @@
                     <div class="mb-3">
                         <label for="cv" class="form-label">Curriculum</label>
                         <input class="form-control" type="file" id="cv" name="cv">
+                        <!-- profile cv preview -->
+                     <div class="preview pt-2">
+                        <object id="file-cv-preview" type="application/pdf" width="20%" class="border border-0 rounded"></object>
+                    </div>
+                    <!-- /profile cv preview -->
                     </div>
                     <div class="mb-3">
                         <label for="image" class="form-label">Profile image</label>
                         <input class="form-control" type="file" id="image" name="image">
+                        <!-- profile image preview -->
+                        <div class="preview pt-2">
+                            <img id="file-image-preview" width="20%" class="rounded">                        
+                        </div>
+                        <!-- /profile image preview -->
                     </div>
 
                     @if ($errors->any())
