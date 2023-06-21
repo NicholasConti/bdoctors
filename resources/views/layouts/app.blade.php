56e2ceace2123d94676dae3d5f2690da5b21e2f7
FerrariMarc0 <marco1010@live.it>
2023-06-21 18:02:33 +0200

    clean-code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,7 +90,7 @@
                                     {{-- <a class="dropdown-item" href="{{ route('admin.') }}">{{__('Profile')}}</a> --}}
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
+                                        document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -100,7 +100,7 @@
                                         {{-- <a class="dropdown-item" href="{{ route('admin.') }}">{{__('Profile')}}</a> --}}
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
+                                        document.getElementById('logout-form').submit();">
                                             {{ __('Logout') }}
                                         </a>
 
@@ -109,6 +109,7 @@
                                             @csrf
                                         </form>
                                     </div>
+                                </div>
                             </li>
                         @endguest
                     </ul>
