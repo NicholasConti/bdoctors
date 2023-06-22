954e66212498753a829155de1324dcd92f4422b7
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-06-22 10:30:41 +0200

    fix:store

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -8,9 +8,19 @@ use Illuminate\Http\Request;
 class MessageController extends Controller
 {
     public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:40',
+            'surname' => 'required|string|max:40',
+            'email' => 'required|email|max:70',
+            'text_message' => 'required|string',
+            'doctor_id' => 'required|integer|exists:doctors,id'
+        ]);
+
+        $data=$request->all();
+
         return response()->json([
             'success' => true,
-            'results' => ''
+            'results' => $data
         ]);
     }
 }
