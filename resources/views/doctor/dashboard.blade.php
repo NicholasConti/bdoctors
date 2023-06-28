fe8b7e711d5735e2d36057fa8045bf3252ed93d6
FerrariMarc0 <marco1010@live.it>
2023-06-28 10:32:23 +0200

    clean-code

--- resources/views/doctor/dashboard.blade.php
+++ resources/views/doctor/dashboard.blade.php
@@ -323,8 +323,7 @@
                     <div class="d-flex my-5 pt-3 border-top" style="margin: 0 100px">
                         <div class="mb-3 w-100 me-5 border-end pe-5" style="margin-left: 100px">
                             <label for="cv" class="form-label">Curriculum</label>
-                            <input class="form-control" type="file" id="cv" name="cv"
-                                style="width: 400px">
+                            <input class="form-control" type="file" id="cv" name="cv" style="width: 400px">
                             <!-- profile cv preview -->
                             <div class="preview pt-2 text-center">
                                 {{-- <object data="{{ $doc->cv }}" type="application/pdf" width="20%" class="border border-dark rounded"></object> --}}
